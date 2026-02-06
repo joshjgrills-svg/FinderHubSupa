dee9c9108b09feca988b1ab1d3b73c89c1da77de
joshjgrills-svg <joshjgrills@gmail.com>
2026-02-06 15:45:11 -0500

    feat: add MethodologyPage stub

---
+++ components/pages/MethodologyPage.tsx
@@ -0,0 +1,11 @@
+// components/pages/MethodologyPage.tsx
+import React from 'react';
+
+export const MethodologyPage: React.FC = () => {
+  return (
+    <section style={{ padding: 24 }}>
+      <h2>Methodology</h2>
+      <p>Preview stub for Methodology page.</p>
+    </section>
+  );
+};
