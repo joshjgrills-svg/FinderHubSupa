a9e32d0895a3852045742510592607f2fc8255f3
joshjgrills-svg <joshjgrills@gmail.com>
2026-02-06 16:00:57 -0500

    feat: add AboutPage stub

--- components/pages/AboutPage.tsx
+++ components/pages/AboutPage.tsx
@@ -5,7 +5,7 @@ export const AboutPage: React.FC = () => {
   return (
     <section style={{ padding: 24 }}>
       <h2>About</h2>
-      <p>Preview stub for About Page.</p>
+      <p>Preview stub for About page.</p>
     </section>
   );
 };
